61fdc4577f8091beef0e06896448dc72059aa162
Dmitriy Paramonov <divniy@gmail.com>
2023-05-05 14:15:14 +0300

    Add ball transparency

--- Assets/Scripts/BallFacade.cs
+++ Assets/Scripts/BallFacade.cs
@@ -3,16 +3,25 @@ using Zenject;
 
 namespace Diwide.Arkanoid
 {
-    public class BallFacade
+    public class BallFacade : IInitializable
     {
         [Inject] private BallMover _ballMover;
         [Inject] public Transform transform;
+        [Inject] private MeshRenderer _renderer;
+        private Color _originColor;
+        private Color _transparentColor;
 
+        public void Initialize()
+        {
+            _originColor = _renderer.material.color;
+            _transparentColor = new Color(_originColor.r, _originColor.g, _originColor.b, .5f);
+        }
         public void ResetToPlayer(PlayerFacade playerFacade)
         {
             _ballMover.StopMoving();
             transform.SetParent(playerFacade.transform, false);
             transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            _renderer.material.color = _transparentColor;
         }
 
         public void Launch()
@@ -20,6 +29,7 @@ namespace Diwide.Arkanoid
             if(_ballMover.IsMoving) return;
             Debug.Log("Launch ball");
             transform.SetParent(null);
+            _renderer.material.color = _originColor;
             _ballMover.StartMoving();
         }
 

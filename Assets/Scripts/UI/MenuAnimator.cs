facf1c09c4dcb9b76cb9e18e68b4e9df8b17e99c
Dmitriy Paramonov <divniy@gmail.com>
2023-05-13 22:56:47 +0300

    Disable interactions on menu animation

--- Assets/Scripts/UI/MenuAnimator.cs
+++ Assets/Scripts/UI/MenuAnimator.cs
@@ -1,19 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
 namespace Diwide.Arkanoid.UI
 {
     public class MenuAnimator : MonoBehaviour
     {
-        private void OnEnable()
+        [Inject] private MenuManager _menuManager;
+        [SerializeField] private bool _animatedOnStart = false;
+        private float _duration = 1f;
+        public Selectable[] _interactables;
+
+        private void Start()
         {
-            transform.DOScale(Vector3.one, 3).From(Vector3.zero, true);
+            _interactables = GetComponentsInChildren<Selectable>();
+            if(_animatedOnStart) Open();
         }
 
-        private void OnDisable()
+        public void Open()
         {
-            transform.DOScale(Vector3.zero, 3).From(Vector3.one, true);
+            Tween tween = transform.DOScale(Vector3.one, _duration).From(Vector3.zero);
+            InteractionsWrapper(tween);
+            _menuManager.OpenMenu(tween);
+        }
+
+        public void Close()
+        {
+            Tween tween = transform.DOScale(Vector3.zero, _duration).From(Vector3.one);
+            InteractionsWrapper(tween);
+            _menuManager.CloseMenu(tween);
+        }
+
+        private void InteractionsWrapper(Tween tween)
+        {
+            if (_interactables.Any())
+            {
+                tween
+                    .OnStart(() => SetInteractionsAvailability(false))
+                    .OnComplete(() => SetInteractionsAvailability(true));
+            }
+        }
+
+        private void SetInteractionsAvailability(bool value)
+        {
+            foreach (var control in _interactables)
+            {
+                control.interactable = value;
+            }
         }
     }
 }
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine;
+using Zenject;
+
+namespace Diwide.Arkanoid.UI
+{
+    public class MenuManager : IInitializable
+    {
+        [Inject] private List<MenuAnimator> _menuAnimators;
+        private Sequence _sequence;
+
+        public Sequence Sequence
+        {
+            get {
+                if (_sequence == null || !_sequence.IsActive())
+                {
+                    _sequence = DOTween.Sequence();
+                }
+                return _sequence;
+            }
+        }
+
+        public void Initialize()
+        {
+            foreach (var hidden in _menuAnimators.FindAll(_=> !_.isActiveAndEnabled))
+            {
+                hidden.transform.localScale = Vector3.zero;
+                hidden.gameObject.SetActive(true);
+            }
+        }
+
+        public void OpenMenu(Tween tween)
+        {
+            Sequence.Append(tween);
+        }
+
+        public void CloseMenu(Tween tween)
+        {
+            Sequence.Prepend(tween);
+        }
+    }
+}
